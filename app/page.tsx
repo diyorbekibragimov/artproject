9f2dd13df0834843ce41053434f91dae1384a34b
Ulan Seitkaliyev <ulanseitkaliyev@cmu.edu>
2024-02-17 01:56:14 +0300

    init

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,108 @@
-import Image from "next/image";
+import { FC } from "react";
 
-export default function Home() {
+const HomePage: FC = () => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="h-min-full h-full min-h-dvh bg-black">
+      <header className="w-full relative flex flex-col justify-between py-5 text-center text-white">
+        <div className="absolute inset-0 bg-hero bg-cover blur-sm"></div>
+
+        <div className="start w-full z-10 flex flex-row justify-between px-20">
+          <div className="w-3/5 text-start">
+            <h1 className="text-3xl font-title font-extrabold uppercase tracking-wide">
+              Arts & Crafts club
+            </h1>
+          </div>
+          <div className="w-2/5 m-auto">
+            <nav className="flex justify-end mr-4 uppercase tracking-wider">
+              <a href="#" className="hover:underline mr-20">
+                Home
+              </a>
+              <a href="#" className="hover:underline mr-20">
+                Submit
+              </a>
+              <a href="#" className="hover:underline mr-10">
+                About Us
+              </a>
+            </nav>
+          </div>
         </div>
-      </div>
+        <div className="w-full z-10 mt-16 mb-24 px-10 tracking-tight">
+          <h1 className="text-4xl md:text-[110px]/[100px] text-start font-extrabold font-extra-condensed uppercase">
+            Beneath the rubble, dreams persist:
+            <span className="block">A tribute to resilient artists</span>
+          </h1>
+        </div>
+      </header>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      {/* <main className="flex flex-col items-center w-full flex-1 px-20 text-center">
+        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
+          <div className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600">
+            <h3 className="text-2xl font-bold">Submit &rarr;</h3>
+            <p className="mt-4 text-xl">
+              Your participation matters! Contribute to our online gallery
+              dedicated to Gaza&apos;s martyred artists. Click here to submit an
+              artist from Gaza and help preserve their creative legacy.
+              Let&apos;s honor their lives and dreams together.
+            </p>
+          </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+          <div className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600">
+            <h3 className="text-2xl font-bold">About Gallery &rarr;</h3>
+            <p className="mt-4 text-xl">
+              This gallery honors Gaza&apos;s martyred artists, fulfilling their
+              dream of having a space to showcase their creativity despite the
+              ongoing genocide. Each piece is a tribute to their lives, echoing
+              their dreams and resilience. It stands as a beacon of hope and a
+              call for justice in the face of adversity.
+            </p>
+          </div>
+        </div>
+      </main> */}
+      <main className="h-auto bg-black">
+        <div className="flex flex-col">
+          <div className="flex flex-row border-white border-b-2 font-extra-condensed text-white px-10">
+            <div className="w-[40%]  text-2xl font-normal tracking-wide border-r-2 uppercase pt-10 pb-2">
+              Submit
+            </div>
+            <div className="w-[60%]  text-2xl font-normal tracking-wide uppercase px-10 pt-10 pb-2">
+              About Gallery
+            </div>
+          </div>
+          <div className="flex mt-auto flex-row border-white font-extra-condensed text-white px-10">
+            <div className="w-[40%] font-normal font-body tracking-wide border-r-2 pr-10">
+              <p className="mt-4 text-sm">
+                Your participation matters! Contribute to our online gallery
+                dedicated to Gaza&apos;s martyred artists. Click here to submit
+                an artist from Gaza and help preserve their creative legacy.
+                Let&apos;s honor their lives and dreams together.
+              </p>
+            </div>
+            <div className="w-[60%] font-normal font-body tracking-wide px-10 pb-2">
+              <p className="mt-4 text-sm">
+                This gallery honors Gaza&apos;s martyred artists, fulfilling
+                their dream of having a space to showcase their creativity
+                despite the ongoing genocide. Each piece is a tribute to their
+                lives, echoing their dreams and resilience. It stands as a
+                beacon of hope and a call for justice in the face of adversity.
+              </p>
+            </div>
+          </div>
+          <div className="flex flex-row border-white font-extra-condensed text-white px-10">
+            <div className="w-[40%] pb-24 font-normal font-body tracking-wide border-r-2 pr-10">
+              <p className="mt-5 text-2xl cursor-pointer font-extra-condensed hover:underline">
+                <a className="">click here to submit to the online gallery</a>
+              </p>
+            </div>
+            <div className="w-[60%] pb-24 font-normal font-body tracking-wide px-10">
+              <p className="mt-5 text-2xl cursor-pointer font-extra-condensed hover:underline">
+                <a className="">click here to start online gallery</a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </main>
+    </div>
   );
-}
+};
+
+export default HomePage;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,11 +7,17 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    fontFamily: {
+      'title': ['var(--font-saira-condensed)'],
+      'body': ['var(--font-inter)'],
+      'extra-condensed': ['var(--font-saira-extra-condensed)']
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'hero': 'url("/bgpage.jpg")',
       },
     },
   },
