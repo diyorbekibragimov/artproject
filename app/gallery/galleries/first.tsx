3dbedeaf427fca59803e3ad07d586d0b9bf857f6
Ulan Seitkaliyev <ulanseitkaliyev@cmu.edu>
2024-04-20 15:51:37 +0300

    rifaat updated

--- app/gallery/galleries/first.tsx
+++ app/gallery/galleries/first.tsx
@@ -5,15 +5,6 @@ const First = () => {
   return (
     <div className="py-5 md:py-20 min-h-full h-full">
       <div className="flex-col relative md:flex-row flex w-full items-center">
-      <div className="md:absolute hidden md:block -mt-10 top-0 right-0">
-          <Image
-            src="/first-two.jpg"
-            alt=""
-            width="250"
-            height="150"
-            objectFit="cover"
-          />
-        </div>
         <div className="w-full md:w-2/5">
           <Image
             src="/first-main.jpg"
@@ -24,8 +15,9 @@ const First = () => {
           />
         </div>
         <div className="w-full p-10 md:p-0 md:w-3/5">
-
-          <div className="uppercase bg-black md:py-4 px-10 tracking-wide md:-ml-20">mohammed sami</div>
+          <div className="uppercase bg-black md:py-4 px-10 tracking-wide md:-ml-20">
+            mohammed sami
+          </div>
           <p className="text-white mt-4 font-bold text-center text-3xl md:text-6xl font-title md:-ml-48">
             “He loved life, he loved Gaza and he loved his story.”
           </p>
